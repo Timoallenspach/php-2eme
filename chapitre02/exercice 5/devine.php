bb16c0f05502a024c65ca04763823ae18af1366c
cpnv <cpnv@cpnv.ch>
2015-04-02 14:34:34 +0200

    message

--- chapitre02/exercice 5/devine.php
+++ chapitre02/exercice 5/devine.php
@@ -1,28 +1,25 @@
 <?php
-$nombre = 3;
 const NBR_MAX = '5';
 const NBR_MIN = '1';
 $nombre = rand(NBR_MIN, NBR_MAX);
 ?>
     
 <!DOCTYPE html>
-
 <html>
 	<head>
 		
 		<meta charset="utf-8">
-        
-    <title>Jeu</title>
+        <title>Jeu</title>
     
     </head>
     
 	<body>
 	
     <h1>Jeu de devinette</h1>
-    <form name="form1" method="post" action="resutat.php">
+    <form name="form1" method="post" action="resultat.php">
         <input type="text" name="nbr" />
         <input type="submit" value="tester" />
-        <input type="hidden" name="valeurADeviner" value="<?=$nombre ?>" /> 
+        <input type="hidden" name="valeurADeviner" value="<?=$nombre ?>"/> 
     </form>
 
   	</body>
